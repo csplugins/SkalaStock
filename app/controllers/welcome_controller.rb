bd211552964ddb002401005b2ba2995b42521b08
Cody <cspluggs@gmail.com>
2016-08-16 21:26:40 -0400

    Successfully fetching all of NYSE symbols and searching them for desired numbers. Still need to add the percent of the 52 wk range.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,11 +8,33 @@ class WelcomeController < ActionController::Base
   end
 
   def search
-    #render html: '<b>html goes here<b/>'.html_safe
+    if params[:q].present? == false
+      params[:q] = 123
+    end
+    if params[:r].present? == false
+      params[:r] = 123
+    end
+    if params[:s].present? == false
+      params[:s] = 123
+    end
     yahoo_client = YahooFinance::Client.new
-    data = yahoo_client.quotes(["AAPL", "AAPL", "AAPL"], [:ask, :symbol])
-    @data = data[0].ask
-    @dataSymbol = data[0].symbol
+    #data = yahoo_client.quotes(["AAPL", "AAPL", "AAPL"], [:ask, :symbol])
+    data = yahoo_client.quotes(yahoo_client.symbols_by_market('us', 'nyse'), [:ask, :symbol, :pe_ratio, :market_capitalization, :dividend_yield, :high_52_weeks, :low_52_weeks ])
+    @dataSymbol = []
+    for item in data
+      next if item.pe_ratio.present? == false
+      next if item.market_capitalization.present? == false
+      next if item.dividend_yield.present? == false
+      next if item.pe_ratio > params[:r]
+      next if item.market_capitalization < params[:q]
+      next if item.dividend_yield < params[:s]
+      next if item.dividend_yield == "N/A"
+
+      puts item.dividend_yield
+
+      @dataSymbol.push(item)
+    end
+    #puts @dataSymbol.count
   end
 
 end
